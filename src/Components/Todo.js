d8122388dd87e3afe82d60ad5b912853efe7faef
Ojhaakshat <ojhaakshat123@gmail.com>
2022-01-21 20:01:35 +0530

    functions to change state

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,19 +5,42 @@ export default class Todo extends Component {
         super();
         this.state = {
             currTask : '',
-            tasks: [{task: 'F o', id: 1}, {task: 'f i', id: 2}]
+            tasks: []
         }
     }
-    
+    handleChange = (e) => {
+        this.setState({
+            currTask: e.target.value
+        })
+    }
+    handleSubmit = () => {
+        this.setState({
+            tasks: [...this.state.tasks, {task: this.state.currTask,id: this.state.tasks.length+1}],
+            currTask: ''
+        })
+    }
+    handleDelete = (id) => {
+        let narr = this.state.tasks.filter((ele) => (
+            ele.id !== id
+        ))
+        this.setState({
+            tasks: [...narr]
+        })
+    }
     render() {
         return(
             <React.Fragment>
-                <input type='text'></input>
-                <button>Submit</button>
+                <input type='text'  value={this.state.currTask} onChange={this.handleChange}></input>
+                <button onClick={this.handleSubmit}>Add</button>
                 <ul>
                     {
                         this.state.tasks.map((ele) => {
-                            return <li key = {ele.id}><p>{ele.task}</p><button>Delete</button></li>
+                            return (<li key = {ele.id}>
+                                        <p>{ele.task}</p>
+                                        {/* here we don't want handleDelete to be called when dom is rendered, so in
+                                        onclick we pass its definition instead of function call hence made it arro function*/}
+                                        <button onClick={() => this.handleDelete(ele.id)}>Delete</button>
+                                    </li>)
                         })
                     }
                 </ul>
